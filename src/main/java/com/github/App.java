fe58c4b195b620c63f7a6285e2dbaa50c844a045
chenghongyan <chenghy.js@eakay.cn>
2018-05-31 10:44:36 +0800

    tortoiseGit

--- src/main/java/com/github/App.java
+++ src/main/java/com/github/App.java
@@ -1,4 +1,4 @@
-package com.github;
+﻿package com.github;
 
 /**
 * @Title: App
@@ -11,7 +11,7 @@ public class App
     /**
      * 版本号
      */
-    public static String version="1.0";
+    public static String version="1.1";
 
     public static void main( String[] args )
     {
